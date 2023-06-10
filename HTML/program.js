784dfcc49b866f07f12398b4bfe859d37d913aa5
kvelraj_concord <kvelraj@concord.net>
2023-06-10 12:54:52 +0530

    commit message

---
+++ HTML/program.js
@@ -0,0 +1,16 @@
+// Number -> Any Numerical value from Positive, Negative, Fractional, Deciaml 
+
+// String -> Any Text value which is typed inside '' and ""
+
+// Boolean -> Either true or false 
+
+// Varaible Creation (Temporary storage)
+// syntax 
+// var vaiable_name = value;
+
+// var -> Inbuild keyword 
+// vaiable_name -> Any valid text, it should contain a-z, A-Z, 0-9, _, $
+// value -> All type of data
+
+var user_emailid = "testuser@demo.in";
+var user_rollnumber = 7555;
