ac7a47c4b58ae43b9c9d414aadc7ac658d884439
kvelraj_concord <kvelraj@concord.net>
2023-07-09 12:40:20 +0530

    commit message

--- learning-react/src/screens/login.js
+++ learning-react/src/screens/login.js
@@ -1,10 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 const LoginScreen = () => {
+
+  const [ loginForm, updateLoginForm ] = useState({
+    username : "",
+    password : ""
+  })
+
+  const handleInput = (event) => {
+    console.log(event.target.value, event.target.id);
+    
+    if(event.target.id == "username"){
+      updateLoginForm({...loginForm, username :event.target.value });
+    }
+    else{
+      updateLoginForm({...loginForm, password :event.target.value });
+    }
+  }
+
+  const submitLogin = () => {
+    console.log(loginForm);
+  }
+
   return(
     <div>
       <h1>This is a Login Screen</h1>
+      <div>
+        <label>Enter your UserName</label>
+        <input type="text" placeholder="Enter username" id="username" onChange={handleInput} />
+      </div>
+      <div>
+        <label>Enter your Password</label>
+        <input type="password" placeholder="Enter password" id="password" onChange={handleInput} />
+      </div>
+      <div>
+        <button onClick={() => submitLogin()}>Login</button>
+      </div>
+
       <NavLink to="/">Back to Landing Screen</NavLink>
     </div>
   )
--- learning-react/src/screens/todo-list.js
+++ learning-react/src/screens/todo-list.js
@@ -1,42 +1,44 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
 const TodoListScreen = () => {
-
-  const [ todoList, updateToDoList ] = useState([]);
+  const [todoList, updateToDoList] = useState([]);
   const [message, updateMessage] = useState("");
 
   const onHandleInput = (event) => {
     updateMessage(event.target.value);
-  }
+  };
 
   const addToDoMessage = () => {
     updateToDoList([...todoList, message]);
     updateMessage("");
-  }
+  };
 
   const deleteToDoMessage = (index) => {
     todoList.splice(index, 1);
     updateToDoList([...todoList]);
-  }
+  };
 
-  const toDoResult = todoList.map( (value, index) => {
-    return(
+  const toDoResult = todoList.map((value, index) => {
+    return (
       <li key={index}>
         <span>{value}</span>
         <button onClick={() => deleteToDoMessage(index)}>X</button>
       </li>
-    )
-  })
+    );
+  });
 
   return (
     <div>
       <h2>Enter your ToDo List</h2>
-      <input type='text' placeholder='Enter ToDo Message' onChange={onHandleInput} value={message} />
+      <input
+        type="text"
+        placeholder="Enter ToDo Message"
+        onChange={onHandleInput}
+        value={message}
+      />
       <button onClick={() => addToDoMessage()}>Add Message</button>
-      <ol>
-        {toDoResult}
-      </ol>
-    </div>
+      <ol>{toDoResult}</ol>
+    </div> 
   );
 };
 
