4195f68891470a4dca8e226d1dc0b70b53f95f4a
kvelraj_concord <kvelraj@concord.net>
2023-07-22 11:31:37 +0530

    commit message

--- learning-react/src/screens/conditional-rendering.js
+++ learning-react/src/screens/conditional-rendering.js
@@ -1,8 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const ConditionalRendering = () => {
+
+  const [ genderOthers, updateGenderOthers ] = useState(false);
+
+  const changeGender = (event) => {
+    if(event.target.value === 'others'){
+      updateGenderOthers(true);
+    }
+    else{
+     updateGenderOthers(false);
+   }
+  }
+
   return (
     <div>
+      <div className='content'>
+        <label>Select your Gender: </label>
+        <input type='radio' value="male" name='gender' onChange={changeGender}/>Male
+        <input type='radio'value="female" name='gender' onChange={changeGender}/>Female
+        <input type='radio' value="others" name='gender' onChange={changeGender}/>Others
+        {
+          genderOthers && 
+          <div>
+            <label>Select your Pronunce :</label>
+            <select defaultValue={''}>
+              <option disabled value="">Choose pronounce</option>
+              <option>He</option>
+              <option>She</option>
+              <option>They</option>
+            </select>
+          </div>
+        }
+        
+      </div>
+
+
+
+
       {/* { condition && JSX } */}
       { true && <img src={require("../images/shinchu.jpg")} width="300" alt="" /> }
       
