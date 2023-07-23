c3e357460fe8fd4b194c972d9225c2fb8e3467aa
kvelraj_concord <kvelraj@concord.net>
2023-07-23 12:21:47 +0530

    commit message

--- learning-react/src/screens/basic-profile.js
+++ learning-react/src/screens/basic-profile.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import axios from 'axios';
 
 const BasicProfileScreen = () => {
+
+  const loadUserProfile = () => {
+    const url = "https://reqres.in/api/users?page=2"
+    axios.get(url)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+
+  }
+
   return (
     <div>
       <h2>This is a Basic Profile Screen</h2>
+      <button onClick={() => loadUserProfile()}>Load Users</button>
     </div>
   );
 };
