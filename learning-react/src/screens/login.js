7f5f13aceb2516171e2ffe4197de3753bbb7a4e0
kvelraj_concord <kvelraj@concord.net>
2023-07-23 10:46:43 +0530

    commit message

--- learning-react/src/screens/login.js
+++ learning-react/src/screens/login.js
@@ -8,6 +8,11 @@ const LoginScreen = () => {
     password : ""
   })
 
+  const [loginFormError, updateLoginFormError] = useState({
+    UserName : false,
+    password : false
+  })
+
   const handleInput = (event) => {
     //console.log(event.target.value, event.target.id);
     updateLoginForm({...loginForm, [event.target.id] :event.target.value });
@@ -15,6 +20,22 @@ const LoginScreen = () => {
 
   const submitLogin = () => {
     console.log(loginForm);
+
+    // if(loginForm.UserName === ""){
+    //   updateLoginFormError({...loginFormError, UserName : true});
+    // }
+    // if(loginForm.password === ""){
+    //   updateLoginFormError({...loginFormError, password : true});
+    // }
+
+    updateLoginFormError({
+      ...loginFormError,
+      UserName : loginForm.UserName === "" ? true : false,
+      password : loginForm.password === "" ? true : false 
+    })
+
+
+    //alert("Form has been submitted to Database");
   }
 
   return(
@@ -23,10 +44,14 @@ const LoginScreen = () => {
       <div>
         <label>Enter your UserName</label>
         <input type="text" placeholder="Enter username" id="UserName" onChange={handleInput} />
+        { loginFormError.UserName &&  <span className="error-msg">Please enter a valid username</span>}
+        
       </div>
       <div>
         <label>Enter your Password</label>
         <input type="password" placeholder="Enter password" id="password" onChange={handleInput} />
+        { loginFormError.password && <span className="error-msg">Please enter a valid passowrd</span>}
+        
       </div>
       <div>
         <button onClick={() => submitLogin()}>Login</button>
