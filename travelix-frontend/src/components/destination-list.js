2ea701e358f9ed1a9eb7bf0b5147d3a37549326f
karthickvkumar <karthickvelraj@live.in>
2023-09-03 11:55:21 +0530

    commit message

--- travelix-frontend/src/components/destination-list.js
+++ travelix-frontend/src/components/destination-list.js
@@ -3,11 +3,11 @@ import React from 'react';
 const DestinationComponent = (props) => {
     // console.log(props);
     return (
-        <div class="col-md-4 m-btm-40">
-            <div class="item">
-            <div class="project-destination">
-                <a href="#" class="img" style={{ backgroundImage: `url(${props?.destinaiton?.tour_image})` }}>
-                <div class="text">
+        <div className="col-md-4 m-btm-40">
+            <div className="item">
+            <div className="project-destination">
+                <a href="#" className="img" style={{ backgroundImage: `url(${props?.destinaiton?.tour_image})` }}>
+                <div className="text">
                     <h3>{props?.destinaiton?.tour_location}</h3>
                     <span>{props?.destinaiton?.tour_count} Tours</span>
                 </div>
--- travelix-frontend/src/components/hotel-list.js
+++ travelix-frontend/src/components/hotel-list.js
@@ -2,26 +2,26 @@ import React from 'react';
 
 const HotelListComponent = (props) => {
     return (
-        <div class="col-md-4">
-              <div class="project-wrap hotel">
-                <a href="#" class="img" style={{ backgroundImage: `url(${props.hotel.hotel_image})` }}>
-                  <span class="price">${props?.hotel?.hotel_price}/person</span>
+        <div className="col-md-4">
+              <div className="project-wrap hotel">
+                <a href="#" className="img" style={{ backgroundImage: `url(${props?.hotel?.hotel_image})` }}>
+                  <span className="price">${props?.hotel?.hotel_price}/person</span>
                 </a>
-                <div class="text p-4">
-                  <p class="star mb-2">
-                    <span class="fa fa-star"></span>
-                    <span class="fa fa-star"></span>
-                    <span class="fa fa-star"></span>
-                    <span class="fa fa-star"></span>
-                    <span class="fa fa-star"></span>
+                <div className="text p-4">
+                  <p className="star mb-2">
+                    <span className="fa fa-star"></span>
+                    <span className="fa fa-star"></span>
+                    <span className="fa fa-star"></span>
+                    <span className="fa fa-star"></span>
+                    <span className="fa fa-star"></span>
                   </p>
-                  <span class="days">8 Days Tour</span>
+                  <span className="days">8 Days Tour</span>
                   <h3><a href="#">{props?.hotel?.hotel_name}</a></h3>
-                  <p class="location"><span class="fa fa-map-marker"></span>{props?.hotel?.hotel_location}</p>
+                  <p className="location"><span className="fa fa-map-marker"></span>{props?.hotel?.hotel_location}</p>
                   <ul>
-                    <li><span class="flaticon-shower"></span>{props?.hotel?.room_count}</li>
-                    <li><span class="flaticon-king-size"></span>{props?.hotel?.room_bed_count}</li>
-                    <li><span class="flaticon-mountains"></span>{props?.hotel?.room_facing}</li>
+                    <li><span className="flaticon-shower"></span>{props?.hotel?.room_count}</li>
+                    <li><span className="flaticon-king-size"></span>{props?.hotel?.room_bed_count}</li>
+                    <li><span className="flaticon-mountains"></span>{props?.hotel?.room_facing}</li>
                   </ul>
                 </div>
               </div>
